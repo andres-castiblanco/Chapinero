185e5e6f7c11dfe1ea5f3e62491f9a3fff836e5b
andres-castiblanco <37271641+andres-castiblanco@users.noreply.github.com>
2019-10-02 09:40:29 -0500

    Add files via upload

---
+++ data/EdificiosPriorizados_7.js
@@ -0,0 +1,22 @@
+var json_EdificiosPriorizados_7 = {
+"type": "FeatureCollection",
+"name": "EdificiosPriorizados_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "1. 1075 - PREMIER II", "descriptio": "1" }, "geometry": { "type": "Point", "coordinates": [ -74.063949099688543, 4.632627300378071 ] } },
+{ "type": "Feature", "properties": { "Name": "2. 522 - RIVIERA PARQUE 93", "descriptio": "2" }, "geometry": { "type": "Point", "coordinates": [ -74.048686549983614, 4.676412125566078 ] } },
+{ "type": "Feature", "properties": { "Name": "3. 2730 - CONJUNTO RESIDENCIAL PUNTO 52", "descriptio": "3" }, "geometry": { "type": "Point", "coordinates": [ -74.063283599876726, 4.638599199919954 ] } },
+{ "type": "Feature", "properties": { "Name": "4. 629 - CONJUNTO RESIDENCIAL ARBOS", "descriptio": "4" }, "geometry": { "type": "Point", "coordinates": [ -74.06023385963195, 4.638133009705799 ] } },
+{ "type": "Feature", "properties": { "Name": "5. 2811 - FIJAR CENTRO 93B", "descriptio": "5" }, "geometry": { "type": "Point", "coordinates": [ -74.052365199959596, 4.679065899983971 ] } },
+{ "type": "Feature", "properties": { "Name": "6. 1831 - DE USO RESIDENCIAL EL VIRREY", "descriptio": "6" }, "geometry": { "type": "Point", "coordinates": [ -74.06142030035484, 4.643289499381643 ] } },
+{ "type": "Feature", "properties": { "Name": "7. 1417 - CALLE 49", "descriptio": "7" }, "geometry": { "type": "Point", "coordinates": [ -74.066041799719741, 4.636127500296119 ] } },
+{ "type": "Feature", "properties": { "Name": "8. 65 - CONJUNTO RESIDENCIAL EMAUS", "descriptio": "8" }, "geometry": { "type": "Point", "coordinates": [ -74.053093000079215, 4.652688199999847 ] } },
+{ "type": "Feature", "properties": { "Name": "9. 2496 - TORRE PLAZA 54", "descriptio": "9" }, "geometry": { "type": "Point", "coordinates": [ -74.064956400379828, 4.640982400102824 ] } },
+{ "type": "Feature", "properties": { "Name": "10. 644 - GRAN AVENIDA", "descriptio": "10" }, "geometry": { "type": "Point", "coordinates": [ -74.054033266731409, 4.667388010699759 ] } },
+{ "type": "Feature", "properties": { "Name": "11. 911 - AGRUPACION DE VIVIENDA LOMA DE LA CRUZ", "descriptio": "11" }, "geometry": { "type": "Point", "coordinates": [ -74.053277215080982, 4.648074266048233 ] } },
+{ "type": "Feature", "properties": { "Name": "12. 599 - PARQUE 96 ANTES DELOITTE", "descriptio": "12" }, "geometry": { "type": "Point", "coordinates": [ -74.046846026098336, 4.680889523508192 ] } },
+{ "type": "Feature", "properties": { "Name": "13. 241 - CALLE 73", "descriptio": "13" }, "geometry": { "type": "Point", "coordinates": [ -74.055339799895165, 4.656617300024158 ] } },
+{ "type": "Feature", "properties": { "Name": "14. 2663 - QUANTUM", "descriptio": "14" }, "geometry": { "type": "Point", "coordinates": [ -74.055925000158425, 4.675015700088109 ] } },
+{ "type": "Feature", "properties": { "Name": "15. 3070 - 14X98", "descriptio": "15" }, "geometry": { "type": "Point", "coordinates": [ -74.047867199941066, 4.683171599931044 ] } }
+]
+}
